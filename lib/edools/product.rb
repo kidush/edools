962a278767c82cd2cc180115465f751df02c5e40
Thiago F. Lins <thiago.evandson@gmail.com>
2017-09-27 12:34:26 -0300

    Added SchoolProducts features

---
+++ lib/edools/product.rb
@@ -0,0 +1,17 @@
+module Edools
+  class Product
+    include Edools::ApiBaseRequest
+
+    def self.create(params = {})
+      response = conn.post '/school_products', {school_product: params}
+
+      response.body
+    end
+    
+    def self.all
+      response = conn.get '/school_products'
+
+      response.body
+    end
+  end
+end
